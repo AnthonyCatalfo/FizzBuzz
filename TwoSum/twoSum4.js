401fa1ade3530fc3ac7d2095898ce7949233fe27
AnthonyCatalfo <anthonycatalfo@gmail.com>
2018-10-25 14:32:18 -0400

    More coding fun

--- TwoSum/twoSum4.js
+++ TwoSum/twoSum4.js
@@ -3,7 +3,7 @@ let twoSum = function (x, y) {
 	let diff;
 	for (let i = 0; i < x.length; i++) {
 		diff = y - x[i]
-		if (map[diff]) {
+		if (map[diff]!= undefined) {
 			return [map[diff], i]
 		} else {
 			map[x[i]] = i
